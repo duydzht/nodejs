e12bf1d2ba9215e4e68e05095554566f08a29da0
hoang duy duy <hoangduy241199@gmail.com>
2021-10-27 18:55:07 +0700

    Cài đặt Express.js

---
+++ LAB2.1/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const routes = require('./routes');
+const express = require('express');
+
+
+const app = express();
+
+const server = http.createServer(app)
+
+server.listen(3001);
