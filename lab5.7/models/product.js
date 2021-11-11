01e9f99d5e01aad6e18dd280bbc5ee6d41d0d60b
hoang duy duy <hoangduy241199@gmail.com>
2021-11-11 11:06:20 +0700

    Tìm nạp một Sản phẩm với Điều kiện where

---
+++ lab5.7/models/product.js
@@ -0,0 +1,28 @@
+const Cart = require('./cart');
+const db = require('../util/database');
+
+module.exports = class Product {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save() {
+        return db.execute(
+            'insert into products (title, price, imageUrl, description) values (?, ?, ?, ?)', [this.title, this.price, this.imageUrl, this.description]
+        );
+    }
+
+    static deleteById(id) {}
+
+    static fetchAll() {
+        return db.execute('SELECT * FROM products');
+    }
+
+    static findById(id) {
+        return db.execute('select * from products where products.id = ?', [id]);
+    }
+};
