53d91ce598a8765c846448734c91a10930c4efda
hoang duy duy <hoangduy241199@gmail.com>
2021-11-13 17:28:25 +0700

    Tạo kết nối cơ sở dữ liệu

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const mongoConnect = require('./util/database')
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
