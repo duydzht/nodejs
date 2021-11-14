bb873da35a512b3bb7644494f4aef9bf6c11554e
hoang duy duy <hoangduy241199@gmail.com>
2021-11-14 22:06:46 +0700

    Tìm nạp tất cả các sản phẩm

--- controllers/admin.js
+++ controllers/admin.js
@@ -34,7 +34,7 @@ exports.postAddProduct = (req, res, next) => {
 //   const prodId = req.params.productId;
 //   req.user
 //     .getProducts({ where: { id: prodId } })
-//     // Product.findById(prodId)
+//     // Product.findByPk(prodId)
 //     .then(products => {
 //       const product = products[0];
 //       if (!product) {
@@ -56,7 +56,7 @@ exports.postAddProduct = (req, res, next) => {
 //   const updatedPrice = req.body.price;
 //   const updatedImageUrl = req.body.imageUrl;
 //   const updatedDesc = req.body.description;
-//   Product.findById(prodId)
+//   Product.findByPk(prodId)
 //     .then(product => {
 //       product.title = updatedTitle;
 //       product.price = updatedPrice;
@@ -86,7 +86,7 @@ exports.postAddProduct = (req, res, next) => {
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const prodId = req.body.productId;
-//   Product.findById(prodId)
+//   Product.findByPk(prodId)
 //     .then(product => {
 //       return product.destroy();
 //     })
