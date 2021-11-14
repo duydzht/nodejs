1c1cc043b2285dfd88a8f9ddb3cf3656fd409ac6
hoang duy duy <hoangduy241199@gmail.com>
2021-11-14 21:20:00 +0700

    Tạo sản phẩm

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
-  MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/test?retryWrites=true'
-  )
-    .then(client => {
-      console.log('Connected!');
-      callback(client);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    MongoClient.connect(
+            'mongodb+srv://duyduy-99:anhduy99@cluster0.ngszl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        )
+        .then(client => {
+            console.log('Connected!');
+            _db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
