56d0043884a34444c70831dbf3583b08a21f47da
hoang duy duy <hoangduy241199@gmail.com>
2021-11-15 12:35:22 +0700

    Tìm nạp một sản phẩm duy nhất

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -13,10 +14,10 @@ class Product {
         return db
             .collection('products')
             .insertOne(this)
-            .then(result => {
+            .then((result) => {
                 console.log(result);
             })
-            .catch(err => {
+            .catch((err) => {
                 console.log(err);
             });
     }
@@ -27,13 +28,26 @@ class Product {
             .collection('products')
             .find()
             .toArray()
-            .then(products => {
+            .then((products) => {
                 console.log(products);
                 return products;
             })
-            .catch(err => {
+            .catch((err) => {
                 console.log(err);
             });
+    }
+
+    static findByPk(prodId) {
+        const db = getDb();
+        return db
+            .collection('products')
+            .find({ _id: new mongodb.ObjectId(prodId) })
+            .next()
+            .then(product => {
+                console.log(product);
+                return product;
+            })
+            .catch((err) => console.log(err));
     }
 }
 
