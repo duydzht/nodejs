1c9ab3f6393407411996a3cce0749bb2786509fb
hoang duy duy <hoangduy241199@gmail.com>
2021-11-15 23:07:17 +0700

    Thêm chức năng Thêm vào giỏ hàng

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findByPk('6192400c8346e3539e558c21')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
