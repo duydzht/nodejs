053e05478807d53d00dbec2469309bbfa114d4ea
hoang duy duy <hoangduy241199@gmail.com>
2021-11-16 17:48:08 +0700

    Mongoose Install and connect

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -15,16 +14,16 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const User = require('./models/user');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
     User.findByPk('61930fd25dc05cdb5207c7d4')
-        .then(user => {
+        .then((user) => {
             req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -32,6 +31,12 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-});
+mongoose
+    .connect(
+        'mongodb://127.0.0.1:27017/lab_nodejs_db'
+    )
+    .then((result) => {
+        app.listen(3000);
+        console.log('Connected!');
+    })
+    .catch((err) => console.log(err));
