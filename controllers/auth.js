f13d3f262f8a848134fb38a79b0beaa149ea68e1
hoang duy duy <hoangduy241199@gmail.com>
2021-12-09 08:46:27 +0700

    Thêm chức năng đăng nhập

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,17 +21,28 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
-    User.findOne({ email: email})
+    User.findOne({ email: email })
         .then((user) => {
-            if(!user){
+            if (!user) {
                 return res.redirect('/login');
             }
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save((err) => {
-                console.log(err);
-                res.redirect('/');
-            });
+            bcrypt
+                .compare(password, user.password)
+                .then((doMatch) => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        return req.session.save((err) => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login');
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.redirect('/login');
+                });
         })
         .catch((err) => console.log(err));
 };
@@ -40,15 +51,13 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
-    User.findOne({
-        email: email,
-    })
+    User.findOne({ email: email })
         .then((userDoc) => {
             if (userDoc) {
                 return res.redirect('/signup');
             }
             return bcrypt
-                .hash(password, 10)
+                .hash(password, 12)
                 .then((hashedPassword) => {
                     const user = new User({
                         email: email,
@@ -61,7 +70,9 @@ exports.postSignup = (req, res, next) => {
                     res.redirect('/login');
                 });
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 exports.postLogout = (req, res, next) => {
