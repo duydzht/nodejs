aac9f37159256ff46ca9e7dc679f92210356386e
hoang duy duy <hoangduy241199@gmail.com>
2021-12-11 11:09:46 +0700

    Áp dụng bảo vệ CSRF

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,10 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-const isAuth = require('../middleware/is-auth');
 // /admin/add-product => GET
 router.get('/add-product', isAuth, adminController.getAddProduct);
 
