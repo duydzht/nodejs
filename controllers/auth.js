43db59b8d29a35a86d96ef5858564b3ad7a061df
hoang duy duy <hoangduy241199@gmail.com>
2021-12-19 19:00:32 +0700

    Thiết lập và cơ bản về kiểm tra dữ liệu

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,13 +2,15 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 
+const { validationResult } = require('express-validator/check');
+
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
     if (message.length > 0) {
-        message = message[0]
+        message = message[0];
     } else {
         message = null;
-    };
+    }
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -19,10 +21,10 @@ exports.getLogin = (req, res, next) => {
 exports.getSignup = (req, res, next) => {
     let message = req.flash('error');
     if (message.length > 0) {
-        message = message[0]
+        message = message[0];
     } else {
         message = null;
-    };
+    }
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
@@ -65,10 +67,22 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).render('auth/signup', {
+            path: '/signup',
+            pageTitle: 'Signup',
+            errorMessage: errors.array(),
+        });
+    }
     User.findOne({ email: email })
         .then((userDoc) => {
             if (userDoc) {
-                req.flash('error', 'Email exists already, please pick a different one.');
+                req.flash(
+                    'error',
+                    'Email exists already, please pick a different one.'
+                );
                 return res.redirect('/signup');
             }
             return bcrypt
