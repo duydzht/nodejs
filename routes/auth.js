07d859328fd1e3732049fcb22b8f419e719c6ce2
hoang duy duy <hoangduy241199@gmail.com>
2021-12-19 19:26:47 +0700

    Trình xác thực tích hợp & tùy chỉnh

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,16 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage("Please Enter a valid email address."), authController.postSignup);
+router.post(
+    '/signup',
+    check('email').isEmail().withMessage('Please Enter a valid email address.').custom((value, {req})=>{
+        if(value === 'hoangduy241199@gmail.com'){
+            throw new Error('This Email address if forbidden.')
+        }
+        return true;
+    }),
+    authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
