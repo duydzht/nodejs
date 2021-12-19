8c512da903e6edd945a6cf4ed29f07deb61fed5c
hoang duy duy <hoangduy241199@gmail.com>
2021-12-19 21:50:37 +0700

    Thêm trình kiểm tra dữ liệu

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator/check');
 
 const authController = require('../controllers/auth');
 
@@ -13,12 +13,23 @@ router.post('/login', authController.postLogin);
 
 router.post(
     '/signup',
-    check('email').isEmail().withMessage('Please Enter a valid email address.').custom((value, {req})=>{
-        if(value === 'hoangduy241199@gmail.com'){
-            throw new Error('This Email address if forbidden.')
-        }
-        return true;
-    }),
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please Enter a valid email address.')
+            .custom((value, { req }) => {
+                if (value === 'hoangduy241199@gmail.com') {
+                    throw new Error('This Email address if forbidden.');
+                }
+                return true;
+            }),
+        body(
+            'password',
+            'Please enter your password with only numbers and text and at least 5 characters'
+        )
+            .isLength({ min: 5 })
+            .isAlphanumeric(),
+    ],
     authController.postSignup
 );
 
