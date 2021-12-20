f5328b69e3e8289b2aa49e16c148a49f10f6a05a
hoang duy duy <hoangduy241199@gmail.com>
2021-12-20 18:45:56 +0700

    Dọn dẹp dữ liệu

--- routes/auth.js
+++ routes/auth.js
@@ -19,6 +19,7 @@ router.post(
     body('password', 'Password has to be valid.')
       .isLength({ min: 5 })
       .isAlphanumeric()
+      .trim()
   ],
   authController.postLogin
 );
@@ -41,13 +42,15 @@ router.post(
             );
           }
         });
-      }),
+      })
+      .normalizeEmail(),
     body(
       'password',
       'Please enter a password with only numbers and text and at least 5 characters.'
     )
       .isLength({ min: 5 })
-      .isAlphanumeric(),
+      .isAlphanumeric()
+      .trim(),
     body('confirmPassword').custom((value, { req }) => {
       if (value !== req.body.password) {
         throw new Error('Passwords have to match!');
