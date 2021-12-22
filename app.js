930dbfeaa075120453a856240289448c95ad85d8
hoang duy duy <hoangduy241199@gmail.com>
2021-12-22 19:12:16 +0700

    Lọc tập tin bằng Mimetype

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const path = require('path');
+
 const express = require('express');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
@@ -13,22 +14,33 @@ const User = require('./models/user');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/db_nodejs_lab';
 
+const app = express();
 const store = new MongoDBStore({
     uri: MONGODB_URI,
     collection: 'sessions',
 });
-
 const csrfProtection = csrf();
+
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'images');
     },
     filename: (req, file, cb) => {
-        cb(null, file.filename + '-' + file.originalname);
+        cb(null, new Date().toISOString() + '-' + file.originalname);
     },
 });
 
-const app = express();
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -38,7 +50,9 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single('image'));
+app.use(
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     session({
@@ -48,7 +62,6 @@ app.use(
         store: store,
     })
 );
-
 app.use(csrfProtection);
 app.use(flash());
 
@@ -59,7 +72,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    // throw new Error('sync Dummy');
+    // throw new Error('Sync Dummy');
     if (!req.session.user) {
         return next();
     }
@@ -85,7 +98,7 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-    // res.status(error.httpStatusCode).render(...)
+    // res.status(error.httpStatusCode).render(...);
     // res.redirect('/500');
     res.status(500).render('500', {
         pageTitle: 'Error!',
